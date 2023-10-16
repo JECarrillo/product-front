aeb6a24f12c3808bd132140b3c36cd01e8ed4251
JECarrillo <enrique@adroitoverseas.com>
2023-10-16 10:55:36 -0700

    modificacion de interfaz

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.ts'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
-
